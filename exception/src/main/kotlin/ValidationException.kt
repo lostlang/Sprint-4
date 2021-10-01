65dcb77efb8ccc26dfa1fbb5e0be8f88adebed9b
lostlang <53492781+lostlang@users.noreply.github.com>
2021-10-01 15:42:31 +0300

    Add validations and error codes

--- exception/src/main/kotlin/ValidationException.kt
+++ exception/src/main/kotlin/ValidationException.kt
@@ -1,6 +1,10 @@
 class ValidationException(val errorCode: Array<ErrorCode>) : RuntimeException(errorCode.joinToString(",") { it.msg })
 
 enum class ErrorCode(val code: Int, val msg: String) {
-    INVALID_CHARACTER(100, "Недопустимый символ")
-    // ...
+    INVALID_PHONE(101, "Не корректный номер телефона"),
+    INVALID_DATA(102, "Имя и фамилия должны состоять только из кириллици"),
+    INVALID_EMAIL(103, "Почта должна быть написана на латинице"),
+    INVALID_SNILS(104, "Снилс должен состоять только из цифр"),
+    NULL_VALUE(105, "Значение равно null"),
+    INVALID_LONG(106, "Недопустимая длина значения")
 }
--- exception/src/main/kotlin/Validator.kt
+++ exception/src/main/kotlin/Validator.kt
@@ -2,8 +2,104 @@ abstract class Validator<T> {
     abstract fun validate(value: T?): List<ErrorCode>
 }
 
+
 class PhoneValidator : Validator<String>() {
     override fun validate(value: String?): List<ErrorCode> {
-        return listOf(ErrorCode.INVALID_CHARACTER)
+        val reg = "[78][0-9]+$".toRegex()
+
+        if (value == null)
+            return listOf(ErrorCode.NULL_VALUE)
+
+        if (value.length != 11)
+            return listOf(ErrorCode.INVALID_LONG)
+
+        if (!value.matches(reg))
+            return listOf(ErrorCode.INVALID_PHONE)
+
+        return listOf()
+    }
+}
+
+
+class DataValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        val reg = "[аА-яЯ]+$".toRegex()
+
+        if (value == null)
+            return listOf(ErrorCode.NULL_VALUE)
+
+        if (value.length > 16)
+            return listOf(ErrorCode.INVALID_LONG)
+
+        if (!value.matches(reg))
+            return listOf(ErrorCode.INVALID_DATA)
+
+        return listOf()
+    }
+}
+
+
+class EmailValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        val reg = "^[aA-zZ0-9]+@[aA-zZ]+\\.[aA-zZ]+$".toRegex()
+
+        if (value == null)
+            return listOf(ErrorCode.NULL_VALUE)
+
+        if (value.length > 32)
+            return listOf(ErrorCode.INVALID_LONG)
+
+        if (!value.matches(reg))
+            return listOf(ErrorCode.INVALID_EMAIL)
+
+        return listOf()
+    }
+}
+
+
+class SnilsValidator : Validator<String>() {
+    override fun validate(value: String?): List<ErrorCode> {
+        val regex = "[0-9]+".toRegex()
+
+        if (value == null)
+            return listOf(ErrorCode.INVALID_EMAIL)
+
+        if (value.length != 11)
+            return listOf(ErrorCode.INVALID_LONG)
+
+        if (!value.matches(regex))
+            return listOf(ErrorCode.INVALID_SNILS)
+
+        val snils = stringToIntArray(value.dropLast(2))
+        val controlSum = value.takeLast(2).toInt()
+
+        if (calculateControlSum(snils) != controlSum)
+            return listOf(ErrorCode.INVALID_SNILS)
+
+        return listOf()
+    }
+
+    private fun calculateControlSum(snils: IntArray): Int {
+        var controlSum = 0
+        var index = 9
+
+        for (digit in snils) {
+            controlSum += digit * index
+            index--
+        }
+
+        while (controlSum > 101) {
+            controlSum %= 101
+        }
+
+        if (controlSum == 100 || controlSum == 101) {
+            controlSum = 0
+        }
+
+        return controlSum
+    }
+
+    private fun stringToIntArray(string: String): IntArray {
+        return string.map { it.toString().toInt() }.toIntArray()
     }
 }
