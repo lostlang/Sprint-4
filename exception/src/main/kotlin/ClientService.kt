10e9fe15b642c52adf276452fdae8677556bae84
lostlang <53492781+lostlang@users.noreply.github.com>
2021-10-01 15:53:03 +0300

    Add validation to client data

--- exception/src/main/kotlin/ClientService.kt
+++ exception/src/main/kotlin/ClientService.kt
@@ -10,8 +10,13 @@ class ClientService {
 
     private fun validateClient(client: Client) {
         val errorList = ArrayList<ErrorCode>()
+
+        errorList.addAll(DataValidator().validate(client.firstName))
+        errorList.addAll(DataValidator().validate(client.lastName))
         errorList.addAll(PhoneValidator().validate(client.phone))
-        // ...
+        errorList.addAll(EmailValidator().validate(client.email))
+        errorList.addAll(SnilsValidator().validate(client.snils))
+
         if (errorList.isNotEmpty()) {
             throw ValidationException(*errorList.toTypedArray())
         }
