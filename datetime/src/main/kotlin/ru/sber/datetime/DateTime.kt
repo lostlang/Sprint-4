a00f77fd370ec13309df597dc085bf2b42a21039
lostlang <53492781+lostlang@users.noreply.github.com>
2021-10-02 19:13:29 +0300

    DateTime complete

--- datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
+++ datetime/src/main/kotlin/ru/sber/datetime/DateTime.kt
@@ -4,23 +4,47 @@ import java.time.LocalDateTime
 
 // 1.
 fun getZonesWithNonDivisibleByHourOffset(): Set<String> {
-    return emptySet()
+    val searchedZones = ZoneId.getAvailableZoneIds()
+                        .filter { TimeZone.getTimeZone(it)
+                                  .rawOffset % 3600000 != 0 }
+                        .toSet()
+
+    return searchedZones
 }
+
 
 // 2.
 fun getLastInMonthDayWeekList(year: Int): List<String> {
-    return emptyList()
+    var searchedDays: MutableList<String> = mutableListOf()
+    for (mounth in 1..12) {
+        searchedDays.add(YearMonth.of(year, mounth)
+                         .atEndOfMonth()
+                         .dayOfWeek
+                         .toString())
+    }
+
+    return searchedDays
 }
+
 
 // 3.
 fun getNumberOfFridayThirteensInYear(year: Int): Int {
-    return 0
+    var count = 0
+    var date: LocalDate = LocalDate.of(year - 1, 12,31)
+
+    do  {
+        date = date.with(TemporalAdjusters.next(DayOfWeek.FRIDAY))
+        if (date.dayOfMonth == 13) count++
+    } while (date.isBefore(LocalDate.of(year + 1, 1,1)))
+
+    return count
 }
+
 
 // 4.
 fun getFormattedDateTime(dateTime: LocalDateTime): String {
-    return ""
+    var out = dateTime.format (DateTimeFormatter
+                               .ofPattern("dd MMM yyyy, HH:mm")
+                               .withLocale(Locale.ENGLISH))
+    return out
 }
-
-
-
