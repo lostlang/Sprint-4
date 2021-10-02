5517dfb9ce5d361638d08f841abbcb8958de609f
lostlang <53492781+lostlang@users.noreply.github.com>
2021-10-02 16:05:52 +0300

    PowFactory fix

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -3,7 +3,5 @@ package ru.sber.functional
 import kotlin.math.pow
 
 object PowFactory {
-    fun buildPowFunction(pow: Int): (Int) -> Int {
-        return { it.toDouble().pow(pow).toInt() }
-    }
+    fun buildPowFunction(pow: Int): (Int) -> Int = { it.toDouble().pow(pow).toInt() }
 }
