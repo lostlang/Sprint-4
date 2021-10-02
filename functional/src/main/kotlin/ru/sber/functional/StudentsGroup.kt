fab72a9b9257ee5b1bcc40a987c6412fa5723145
lostlang <53492781+lostlang@users.noreply.github.com>
2021-10-02 16:42:31 +0300

    StudentsGroup complete

--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -2,9 +2,16 @@ package ru.sber.functional
 
 class StudentsGroup {
 
-    lateinit var students: List<Student>
+    var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+    fun filterByPredicate(pred: (Student) -> Boolean): List<Student> = students.filter { pred(it) }
+
+    init {
+        students = listOf(
+            Student("Дмитрий", "Романов", "Александрочич", averageRate = 7.2),
+            Student("Арсений", "Петросян", averageRate = 9.1),
+            Student("Полина", "Самсонова", age = 22, averageRate = 9.9),
+            Student("София", "Краснова", city = "Тула", averageRate = 6.6),
+        )
     }
 }
